08e1adcba10eb9e2a10cbf14736da68faa6d96ae
Bhavyasri <nsbhavya83@gmail.com>
2026-09-22 22:26:29 +0530

    Fix frontend API routes

--- frontend/src/services/analyticsService.js
+++ frontend/src/services/analyticsService.js
@@ -1,6 +1,5 @@
 import api from './api';
-
 export const getAnalytics = async () => {
-  const { data } = await api.get('/analytics');
+  const { data } = await api.get('/api/analytics');
   return data.data;
 };
--- frontend/src/services/taskService.js
+++ frontend/src/services/taskService.js
@@ -8,25 +8,30 @@ export const getTasks = async (filters = {}) => {
       params[key] = value;
     }
   });
-  const { data } = await api.get('/tasks', { params });
+
+  const { data } = await api.get('/api/tasks', { params });
+
   return data; // { success, data: [...tasks], pagination }
 };
 
 export const getTaskById = async (id) => {
-  const { data } = await api.get(`/tasks/${id}`);
+  const { data } = await api.get(`/api/tasks/${id}`);
+
   return data.data;
 };
 
 export const createTask = async (task) => {
-  const { data } = await api.post('/tasks', task);
+  const { data } = await api.post('/api/tasks', task);
+
   return data.data;
 };
 
 export const updateTask = async (id, task) => {
-  const { data } = await api.put(`/tasks/${id}`, task);
+  const { data } = await api.put(`/api/tasks/${id}`, task);
+
   return data.data;
 };
 
 export const deleteTask = async (id) => {
-  await api.delete(`/tasks/${id}`);
+  await api.delete(`/api/tasks/${id}`);
 };
